f084f329da37f85130a64cfb957307ce2d63464a
Daniel Fernandes <74274176+daniel-alves-fernandes@users.noreply.github.com>
2021-08-07 13:48:06 +0100

    Update StataEditor.py

--- StataEditor.py
+++ StataEditor.py
@@ -1,49 +1,41 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import os
-import sublime_plugin
-import sublime
-import codecs
-import tempfile
-import subprocess
-import time
-import re
+import os, codecs, tempfile, subprocess, time, re
+import sublime_plugin, sublime
 
 switch_focus = "activate"
 
 class StataLocal(sublime_plugin.TextCommand):
-	def run(self,edit):
-		sels = self.view.sel()
-		for sel in sels:
-			if len(sel) == 0:
-				word_sel = self.view.word(sel.a)
-			else:
-				word_sel = sel
-			word_str = self.view.substr(word_sel)
-			word_str = "`"+word_str+"'"
-			self.view.replace(edit,word_sel,word_str)
+  def run(self,edit):
+    sels = self.view.sel()
+    for sel in sels:
+      if len(sel) == 0:
+        word_sel = self.view.word(sel.a)
+      else:
+        word_sel = sel
+      word_str = self.view.substr(word_sel)
+      word_str = "`"+word_str+"'"
+      self.view.replace(edit,word_sel,word_str)
 class StataMacro(sublime_plugin.TextCommand):
-	def run(self,edit):
-		sels = self.view.sel()
-		for sel in sels:
-			if len(sel) == 0:
-				word_sel = self.view.word(sel.a)
-			else:
-				word_sel = sel
-			word_str = self.view.substr(word_sel)
-			word_str = "${"+word_str+"}"
-			self.view.replace(edit,word_sel,word_str)
+  def run(self,edit):
+    sels = self.view.sel()
+    for sel in sels:
+      if len(sel) == 0:
+        word_sel = self.view.word(sel.a)
+      else:
+        word_sel = sel
+      word_str = self.view.substr(word_sel)
+      word_str = "${"+word_str+"}"
+      self.view.replace(edit,word_sel,word_str)
 class StataToggle(sublime_plugin.TextCommand):
-	def run(self,edit):
-		sels = self.view.sel()
-		for sel in sels:
-			if len(sel) == 0:
-				word_sel = self.view.word(sel.a)
-			else:
-				word_sel = sel
-			word_str = self.view.substr(word_sel)
-			word_str = "/* "+word_str+" */"
-			self.view.replace(edit,word_sel,word_str)
+  def run(self,edit):
+    sels = self.view.sel()
+    for sel in sels:
+      if len(sel) == 0:
+        word_sel = self.view.word(sel.a)
+      else:
+        word_sel = sel
+      word_str = self.view.substr(word_sel)
+      word_str = "/* "+word_str+" */"
+      self.view.replace(edit,word_sel,word_str)
 
 def get_stata_version():
     cmd = """osascript<< END
@@ -55,15 +47,15 @@ def get_stata_version():
         tell me to get application id "com.stata.stata14"
         set stata to 14
         end try
-		  try
+      try
         tell me to get application id "com.stata.stata15"
         set stata to 15
         end try
-		  try
+      try
         tell me to get application id "com.stata.stata16"
         set stata to 16
         end try
-		  try
+      try
         tell me to get application id "com.stata.stata17"
         set stata to 17
         end try
@@ -116,49 +108,49 @@ class LinesStataInteractive(sublime_plugin.TextCommand):
     os.system(cmd)
 
 class LinesStataDo(sublime_plugin.TextCommand):
-	def run(self, edit):
-		selectedcode = ""
-		sels = self.view.sel()
-		for sel in sels:
-			selectedcode = selectedcode + self.view.substr(sel)
-		if len(selectedcode) == 0:
-			selectedcode = self.view.substr(self.view.line(sel))
-		dofile_path = tempfile.gettempdir()+'/sublime.do'
-		with codecs.open(dofile_path, 'w', encoding='utf-8') as out:
-		    out.write(selectedcode)
-		version, stata_app_id = get_stata_version()
-		cmd = """osascript<< END
-		 tell application id "{0}"
-		 		{1}
-		    DoCommandAsync "do {2}"  with addToReview
-		 end tell
-		 END""".format(stata_app_id,switch_focus,dofile_path)
-		print(cmd)
-		print("stata_app_id")
-		print(stata_app_id)
-		os.system(cmd)
+  def run(self, edit):
+    selectedcode = ""
+    sels = self.view.sel()
+    for sel in sels:
+      selectedcode = selectedcode + self.view.substr(sel)
+    if len(selectedcode) == 0:
+      selectedcode = self.view.substr(self.view.line(sel))
+    dofile_path = tempfile.gettempdir()+'/sublime.do'
+    with codecs.open(dofile_path, 'w', encoding='utf-8') as out:
+        out.write(selectedcode)
+    version, stata_app_id = get_stata_version()
+    cmd = """osascript<< END
+     tell application id "{0}"
+        {1}
+        DoCommandAsync "do {2}"  with addToReview
+     end tell
+     END""".format(stata_app_id,switch_focus,dofile_path)
+    print(cmd)
+    print("stata_app_id")
+    print(stata_app_id)
+    os.system(cmd)
 class LinesStataRun(sublime_plugin.TextCommand):
-	def run(self, edit):
-		selectedcode = ""
-		sels = self.view.sel()
-		for sel in sels:
-			selectedcode = selectedcode + self.view.substr(sel)
-		if len(selectedcode) == 0:
-			selectedcode = self.view.substr(self.view.line(sel))
-		dofile_path = tempfile.gettempdir()+'/sublime.do'
-		with codecs.open(dofile_path, 'w', encoding='utf-8') as out:
-		    out.write(selectedcode)
-		version, stata_app_id = get_stata_version()
-		cmd = """osascript<< END
-		 tell application id "{0}"
-		 	{1}
-		  DoCommandAsync "run {2}" with addToReview
-		 end tell
-		 END""".format(stata_app_id,switch_focus,dofile_path)
-		print(cmd)
-		print("stata_app_id")
-		print(stata_app_id)
-		os.system(cmd)
+  def run(self, edit):
+    selectedcode = ""
+    sels = self.view.sel()
+    for sel in sels:
+      selectedcode = selectedcode + self.view.substr(sel)
+    if len(selectedcode) == 0:
+      selectedcode = self.view.substr(self.view.line(sel))
+    dofile_path = tempfile.gettempdir()+'/sublime.do'
+    with codecs.open(dofile_path, 'w', encoding='utf-8') as out:
+        out.write(selectedcode)
+    version, stata_app_id = get_stata_version()
+    cmd = """osascript<< END
+     tell application id "{0}"
+      {1}
+      DoCommandAsync "run {2}" with addToReview
+     end tell
+     END""".format(stata_app_id,switch_focus,dofile_path)
+    print(cmd)
+    print("stata_app_id")
+    print(stata_app_id)
+    os.system(cmd)
 
 class StataHelpCommand(sublime_plugin.TextCommand):
   def run(self,edit):
